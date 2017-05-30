042f36fd1477f819f60e4eb4f8fad2b1ad799002
phuonghoang713 <phuonghoang713@gmail.com>
2017-05-30 22:19:24 +0700

    add get method for sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users/sign_out', controller: "devise/sessions", action: "destroy"
   resources :articles do
     resources :comments
   end
